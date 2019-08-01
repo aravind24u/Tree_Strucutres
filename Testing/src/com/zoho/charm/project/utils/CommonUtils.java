3cd7f40512bbe2a340f8717dbc40905857fa3385
Aravind Sundar <s.aravind4u@gmail.com>
2019-08-01 12:22:13 +0530

    Usual Back up

--- Testing/src/com/zoho/charm/project/utils/CommonUtils.java
+++ Testing/src/com/zoho/charm/project/utils/CommonUtils.java
@@ -3,7 +3,9 @@ package com.zoho.charm.project.utils;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
@@ -11,11 +13,13 @@ import org.apache.commons.lang3.StringUtils;
 import com.zoho.charm.project.utils.encoder.EncodingConstants;
 
 public class CommonUtils {
-	
+
 	public static final String APOLLO_HOME_DIR = "/home/local/ZOHOCORP/aravind-5939/Desktop/Apollo Integration/CSV/";
-	public static final String PRICING_HOME_DIR = "/home/local/ZOHOCORP/aravind-5939/Desktop/Pricing/";
-	public static final String INVOICE_HOME_DIR = "/home/local/ZOHOCORP/aravind-5939/Desktop/ZInvoice/";
-	
+	public static final String PRICING_HOME_DIR = "/home/med/Aravind/Docs/pricing/";
+	public static final String INVOICE_HOME_DIR = "/home/med/Aravind/Docs/Invoice/";
+
+	private static final DecimalFormat DECIMAL_FORMAT = new DecimalFormat("#.###");;
+
 	public static List<String> loadFile(String fileName) {
 		List<String> contents = new ArrayList<>();
 		BufferedReader reader = null;
@@ -42,15 +46,49 @@ public class CommonUtils {
 
 		return contents;
 	}
-	
-	public static List<String> getPracticeIdsToList(List<String>file) throws Exception{
-		List<String>practiceIds = new ArrayList<>();
-		file.forEach(line ->{
-			if(StringUtils.isNotEmpty(line)) {
+
+	public static List<String> getPracticeIdsToList(List<String> file) throws Exception {
+		List<String> practiceIds = new ArrayList<>();
+		file.forEach(line -> {
+			if (StringUtils.isNotEmpty(line)) {
 				String[] values = line.split(",");
 				practiceIds.add(values[0]);
 			}
 		});
 		return practiceIds;
+	}
+
+	public static HashMap<String, Double> getHashMapFromFile(String fileName) {
+		return getHashMapFromFile(CommonUtils.PRICING_HOME_DIR.concat(fileName), 0, 16, ",");
+	}
+
+	public static HashMap<String, Double> getHashMapFromFile(String fileName, Integer keyIndex, Integer valueIndex,
+			String delimiter) {
+
+		BufferedReader reader = null;
+		HashMap<String, Double> usage = new HashMap<>();
+		System.out.println("Loading file : ".concat(fileName));
+		try {
+			reader = new BufferedReader(new FileReader(fileName));
+
+			String line = reader.readLine();
+			line = reader.readLine();
+
+			while (line != null) {
+				line = line.replaceAll("\"", "");
+				String[] values = line.split(delimiter);
+
+				String practiceId = values[keyIndex];
+				Double monthlyCharge = new Double(DECIMAL_FORMAT.format(new Double(values[valueIndex])));
+
+				usage.put(practiceId, monthlyCharge);
+
+				line = reader.readLine();
+			}
+			System.out.println("Finished loading file : ".concat(CommonUtils.PRICING_HOME_DIR.concat(fileName)));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return usage;
 	}
 }
